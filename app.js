e25bbf3ae5ecddb0956bb5712d56c6bd9b2709d3
Allxie <allxie@way2b1.com>
2017-07-22 16:40:31 -0700

    remove node modules folder

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res)=> {res.send("Hello World")});
+//RESEARCH NODEMON to have your server potentially hot-reload
+
+// const objection = require ('objection'); http://vincit.github.io/objection.js/#getting-started
+// const Model = objection.Model;
+// const Knex = require('knex');
+
+// //Initialie knex connection
+// const knex = Knex({
+// 	client:"",
+// 	useNullAsDefault: true,
+// 	connection: "postgres://localhost:5432/joanna_db",
+// });
+
+// Model.knex(knex);
+
+const allMyData = [
+	{
+		question_id: 1,
+		right_doc: "./blueThing.jpg",
+		wrong_doc: "./greenThing.jpg",
+		question_text: "Which thing is blue?"
+	},
+	{
+		question_id: 2,
+		right_doc: "./computer.jpg",
+		wrong_doc: "./cow.jpg",
+		question_text: "Which thing is electronic?"
+	},
+	{
+		question_id: 3,
+		right_doc: "./chess.jpg",
+		wrong_doc: "./ice_cream.jpg",
+		question_text: "Which thing is difficult?"
+	},
+]
+const makeMyData = ()=>{
+	let num = 2//(allMyData.length * Math.random().floor();
+	console.log("NUM ", num);
+	let myQuestion = allMyData[num];
+	console.log(myQuestion);
+	return myQuestion;
+}
+app.get('/', (req, res)=> {
+
+	let myData = makeMyData();
+	res.send(myData);
+});
 app.listen(3000, ()=>{console.log("Hey I'm listening")}); //new es6 arrow syntax
 
 
